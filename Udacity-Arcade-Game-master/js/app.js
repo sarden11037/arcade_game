2261f2ede980164e7e506adaef5d84e3b8957aab
sarden11037 <severo.7477@gmail.com>
2018-01-18 11:30:23 -0600

    commit with functionality
    program is functional according to specs. will add additional functions later

--- Udacity-Arcade-Game-master/js/app.js
+++ Udacity-Arcade-Game-master/js/app.js
@@ -1,113 +1,107 @@
 // Sets an initial player score of 0.
 let score = 0;
 $("#playerScore").html(`${score}`);
+let minusX = 0;
 
-// Enemies our player must avoid
-class Enemy  {
-    constructor(x, y){
-        
-    
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-    } //end of constructor 
+// Enemies class with appropriate functions
+class Enemy {
+    constructor(x, y) {
+            this.sprite = 'images/enemy-bug.png';
+            this.x = x;
+            this.y = y;
+        } //end of constructor 
     update(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    if (this.x < 505) {
-        this.x += (150 * dt);
+        //update function for position of enemy
+        if (this.x < 505) {
+            this.x += (150 * dt);
+        } else {
+            this.x = -90;
+        }
+
+        // If the enemy and the player collide.
+        if (this.x < player.x + 30 && this.x + 60 > player.x && this.y < player.y + 60 && this.y + 40 > player.y) {
+            score = 0;
+            //jquery insert to change score
+            $("#playerScore").html(`${score}`);
+            player.reset();
+        }
+    }; //end of - update function
+    // Draw the enemy function
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }; // end of draw enmy function
+
+};
+
+
+
+
+
+
+//player class with fixed constructor
+class Player {
+    constructor(x, y) {
+        this.sprite = 'images/char-boy.png';
+        this.x = 200;
+        this.y = 320;
     }
-    else {this.x = -90;}
+    update() {
+        if (player.y < 20) {
+            //if player wins, score will be updated
+            score++;
+            $("#playerScore").html(`${score}`);
+            this.reset();
+        }
+    }; //end-of update function
+    //draw function
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }; //end-of draw function
+    //input function
+    handleInput(direction) {
+        if (direction == 'left' && this.x > 0) {
+            this.x -= 100;
+        }
+        if (direction == 'right' && this.x < 400) {
+            this.x += 100;
+        }
+        if (direction == 'up' && this.y > 3) {
+            this.y -= 100;
+        }
+        if (direction == 'down' && this.y < 400) {
+            this.y += 100;
+        }
+    }; //end-of input function
+    //reset after win/loss
+    reset() {
+        this.x = 200;
+        this.y = 320;
+    }; //end-of reset
 
-	// If the enemy and the player collide.
-    if(this.x < player.x + 30 && this.x + 60 > player.x && this.y < player.y + 60 && this.y + 40 > player.y) {
-		score = 0;
-
-        $("#playerScore").html(`${score}`);
-		player.reset();
-    }
-};
-    
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+}; //end of class
 
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-let Player = function () {
-    this.sprite = 'images/char-boy.png';
-    this.x = 200;
-    this.y = 320;
-};
-
-// Is called every time the player position is updated
-Player.prototype.update = function() {
- 	
-	// If the player reaches the water
-	if (player.y < 20) {
-	score++;
-	
-    $("#playerScore").html(`${score}`);
-	this.reset();
+// array for random start positions
+let minX = [];
+for (let i = 0; i <= 5; i++) {
+    minX[i] = Math.floor(Math.random() * (-90 - -890 + 1)) + -90;
 }
-};
+let enemy1 = new Enemy(minX[0], 60);
+let enemy2 = new Enemy(minX[1], 140);
+let enemy3 = new Enemy(minX[2], 230);
+let enemy4 = new Enemy(minX[3], 140);
+let enemy5 = new Enemy(minX[4], 60);
+let enemy6 = new Enemy(minX[5], 230);
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
-Player.prototype.handleInput = function(direction) {
-    if(direction == 'left' && this.x > 0) {
-        this.x -= 100;
-    }
-    if(direction == 'right' && this.x < 400) {
-        this.x += 100;
-    }
-    if(direction == 'up' && this.y > 3) {
-        this.y -= 100;
-    }
-    if(direction == 'down' && this.y < 400) {
-        this.y += 100;
-    }
-};
 
-// Is called when the player is reset to the starting point
-Player.prototype.reset = function() {
-    this.x = 200;
-    this.y = 320;
-};
 
-// Now instantiate your objects.
-let enemy1 = new Enemy(-90, 60);
-let enemy2 = new Enemy(-190, 140);
-let enemy3 = new Enemy(-290, 230);
-let enemy4 = new Enemy(-390, 140);
-let enemy5 = new Enemy(-490, 60);
-let enemy6 = new Enemy(-890, 230);
-
-// Place all enemy objects in an array called allEnemies
 let allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
 // Place the player object in a variable called player
 let player = new Player();
 
 
-
-// This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     let allowedKeys = {
